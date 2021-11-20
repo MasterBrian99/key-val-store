504a5d21f3c3d44e4a200f56733a979bc7b44ced
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2021-11-20 12:39:49 +0530

    client initialize

--- kv-store-server/src/app.module.ts
+++ kv-store-server/src/app.module.ts
@@ -3,9 +3,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { ValueModule } from './values/values.module';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), RegisterModule, ValueModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    RegisterModule,
+    ValueModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'build'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
